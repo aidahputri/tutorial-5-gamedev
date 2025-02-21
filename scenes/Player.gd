86421f9350a2593eef915685f72512406656a149
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-02-21 11:44:00 +0700

    feat: implement double jump

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 @export var gravity = 200.0
 @export var walk_speed = 200
-@export var jump_speed = -300
+@export var jump_speed = -150
+@export var can_double_jump = false
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
 
 	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed
+		can_double_jump = true
+		
+	if !is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
+		velocity.y = jump_speed
+		can_double_jump = false
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
