0cfff052b5aaca4522ce4ec9ab3d26f076498052
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-02-21 16:06:54 +0700

    feat: add sprites

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,23 +5,57 @@ extends CharacterBody2D
 @export var jump_speed = -150
 @export var can_double_jump = false
 
-func _physics_process(delta):
-	velocity.y += delta * gravity
+@onready var walk_sprite = $Walk
+@onready var jump_sprite = $Jump
+@onready var _animation_player = $AnimationPlayer
 
-	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
-		velocity.y = jump_speed
-		can_double_jump = true
-		
-	if !is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
+func _physics_process(delta):
+	velocity.y += delta * gravity 
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed('ui_up'):
+			velocity.y = jump_speed
+			can_double_jump = true
+			jump()
+		else:
+			land()
+	elif not is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed
 		can_double_jump = false
+		jump()
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
+		#scale.x = -1 
+		changeDir(true)
+		_animation_player.play("walk")
+
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  walk_speed
+		velocity.x = walk_speed
+		#scale.x = 1 
+		changeDir(false)
+		_animation_player.play("walk")
+
 	else:
 		velocity.x = 0
+		_animation_player.stop() #ganti idle nanti 
 
-	# "move_and_slide" already takes delta time into account.
+	if not is_on_floor():
+		if velocity.y < 0:
+			_animation_player.play("jump")
+		else:
+			_animation_player.play("fall")
+
 	move_and_slide()
+
+func jump():
+	walk_sprite.visible = false
+	jump_sprite.visible = true
+
+func land():
+	walk_sprite.visible = true
+	jump_sprite.visible = false
+	
+func changeDir(dir):
+	walk_sprite.flip_h = dir 
+	jump_sprite.flip_h = dir
