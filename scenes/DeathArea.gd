d60f8eed52707d613ee5daa8ec9bcd0a1e1fed4f
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-02-27 22:05:02 +0700

    chore: prettier

--- scenes/DeathArea.gd
+++ scenes/DeathArea.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+
 func _on_DeathArea_body_entered(body: Node2D):
-	if body.name == "Player": 
+	if body.name == "Player":
 		print("Reset level...")
 		call_deferred("reset_level")
+
 
 func reset_level():
 	get_tree().reload_current_scene()
